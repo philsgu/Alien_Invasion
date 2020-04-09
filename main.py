d53f0ef7bf2a0b13ba6b47b1259cd35cf0b74510
PHIL KIM <philsgu@gmail.com>
2020-04-09 15:10:13 +0000

    updated

--- main.py
+++ main.py
@@ -4,34 +4,39 @@ from settings import Settings
 from ship import Ship
 import game_functions as gf
 
+
 class AlienInvasion:
-  """Overall class to manage game assets and behavior."""
+    """Overall class to manage game assets and behavior."""
 
-  def __init__(self):
-    """Initialize the game, and create the game resources."""
-    pygame.init()
+    def __init__(self):
+        """Initialize the game, and create the game resources."""
+        pygame.init()
+        self.settings = Settings()
 
-    self.screen = pygame.display.set_mode((1200, 800))
-    pygame.display.set_caption("Alien Invasion")
+        self.screen = pygame.display.set_mode((self.settings.screen_width,
+                                               self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+        # The self arugment gives Ship access to the game's resources e.g. screen object
+        self.ship = Ship(self)
 
-    # Set the background color:
-    self.bg_color = (230, 230, 230)
+        # Set the background color:
+        self.bg_color = (230, 230, 230)
 
-  def run_game(self):
-    """Start the main loop for the game."""
-    while True:
-      # Watch for keyboard and mouse events.
-      for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-          sys.exit
-      
-      # Redraw the screen during each pass through the loop
-      self.screen.fill(self.bg_color)
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            # Watch for keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit
 
-      # Make the most recently drawn screen visible.
-      pygame.display.flip()
+            # Redraw the screen during each pass through the loop
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
-if __name__ == '__main__':
-  # Make a game instance, and run the game
-  ai = AlienInvasion()
-  ai.run_game()
+            # Make the most recently drawn screen visible.
+            pygame.display.flip()
+
+
+ai = AlienInvasion()
+ai.run_game()
--- ship.py
+++ ship.py
@@ -1,20 +1,21 @@
 import pygame
 
-class Ship():
 
-  def __init__(self, screen):
-    # Initialze the ship and set its starting position
-    self.screen = screen
+class Ship:
+    """A class to manage the ship."""
 
-    # Load the ship image and get its rect
-    self.image = pygame.image.load('images/ship.bmp')
-    self.rect = self.image.get_rect()
-    self.screen_rect = screen.get_rect()
+    def __init__(self, ai_game):
+        # Initialze the ship and set its starting position
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
 
-    # Start each new ship at the bottom center of the screen
-    self.rect.center = self.screen_rect.center
-    self.rect.bottom = self.screen_rect.bottom
-  
-  def blitme(self):
-    # Draw the ship at its current location
-    self.screen.blit(self.image, self.rect)
+        # Load the ship image and get its rect
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+
+        # Start each new ship at the bottom center of the screen
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        # Draw the ship at its current location
+        self.screen.blit(self.image, self.rect)
