42cb062e3c50dd982c1c199e2504683e94a8f65a
PHIL KIM <philsgu@gmail.com>
2020-04-09 05:20:56 +0000

    main file added

--- main.py
+++ main.py
@@ -4,3 +4,12 @@ from settings import Settings
 from ship import Ship
 import game_functions as gf
 
+class AlienInvasion:
+  """Overall class to manage game assets and behavior."""
+
+  def __init__(self):
+    """Initialize the game, and create the game resources."""
+    pygame.init()
+
+    self.screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption("Alien Invasion")
