25ab36dc05ded466f751ceea157c3ad6fb641516
PHIL KIM <philsgu@gmail.com>
2020-04-09 05:17:51 +0000

    Initial commit

---
+++ game_functions.py
@@ -0,0 +1,10 @@
+import sys
+import pygame
+
+def check_events():
+  # Respond to key presses and mouse events
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      sys.exit()
+
+
