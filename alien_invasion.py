ea512355ea82c6ff0ce073ec65b5c75e90866d45
philsgu <philsgu@gmail.com>
2020-04-11 14:14:11 -0700

    completed_12

--- alien_invasion.py
+++ alien_invasion.py
@@ -36,8 +36,19 @@ class AlienInvasion:
             # helper method
             self._check_events()
             self.ship.update()
-            self.bullets.update()
+            self._update_bullets()
             self._update_screen()
+
+    def _update_bullets(self):    
+        """UPdate position of bullets and get rid of old bullets."""
+        # Update bullet positions.
+        self.bullets.update()
+
+        # Get rid of bullet that have disappeared. 
+        for bullet in self.bullets.copy():
+          if bullet.rect.bottom <= 0:
+            self.bullets.remove(bullet)
+        #print(len(self.bullets))
             
 
     # Watch for keyboard and mouse events.
@@ -70,8 +81,10 @@ class AlienInvasion:
 
     def _fire_bullet(self):
       """Create a new bullet and add it to the bullets group"""
-      new_bullet = Bullet(self)
-      self.bullets.add(new_bullet)        
+      if len(self.bullets) < self.settings.bullets_allowed:
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+        
 
     # Redraw the screen during each pass through the loop
     def _update_screen(self):
