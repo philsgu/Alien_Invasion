ebfcc7708a0d38baa6aa7617eb4b15693341a26a
PHIL KIM <philsgu@gmail.com>
2020-04-11 03:39:44 +0000

    edge

--- ship.py
+++ ship.py
@@ -7,10 +7,12 @@ class Ship:
     def __init__(self, ai_game):
         # Initialze the ship and set its starting position
         self.screen = ai_game.screen
-        self.screen_rect = ai_game.screen.get_rect()
-
         # Set setting of the Ship
         self.settings = ai_game.settings
+        
+        self.screen_rect = ai_game.screen.get_rect()
+
+        
 
         # Load the ship image and get its rect
         self.image = pygame.image.load('images/ship.bmp')
@@ -28,10 +30,11 @@ class Ship:
 
     def update(self):
       """Update the ship's position based on flag status"""
-      if self.moving_right:
-        self.rect.x += self.settings.ship_speed
-      if self.moving_left:
-        self.rect.x -= self.settings.ship_speed
+      # UPdate the ship's x value, not the rect
+      if self.moving_right and self.rect.right < self.screen_rect.right:
+        self.x += self.settings.ship_speed
+      if self.moving_left and self.rect.left > 0:
+        self.x -= self.settings.ship_speed
       # Update rect object 
       self.rect.x = self.x
 
